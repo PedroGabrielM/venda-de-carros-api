576043d71bdfce43ab6db7fcb287a5e5ce5f4894
PedroGabrielM <86327526+PedroGabrielM@users.noreply.github.com>
2021-07-23 12:26:35 -0300

    Add files via upload

---
+++ data_range.js
@@ -0,0 +1,8 @@
+    var start = new Date("1990-01-01")
+    var end = new Date("2021-07-23")
+     
+    db.call_archive.find({
+        ano: {
+            $gte: start, $lt: end
+        }
+    }).sort({ano: -1}).pretty()
---
+++ preco_range.js
@@ -0,0 +1,6 @@
+db.call_archive.find({
+        preco: {
+            $gte: 5000, $lte: 200000}
+        }).sort({
+            preco:1
+        })
